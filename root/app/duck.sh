fc84cf9d7fecd17b6fb863d3335fe7f8bb53e421
grutta16 <grutta16@users.noreply.github.com>
2021-02-01 17:59:06 +0100

    Update duck.sh
    Added IPv6 support

--- root/app/duck.sh
+++ root/app/duck.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/with-contenv bash
 
 . /app/duck.conf
-RESPONSE=`curl -s "https://www.duckdns.org/update?domains=$SUBDOMAINS&token=$TOKEN&ip="`
+IP=`curl -s "https://v4.ident.me/"`
+IPV6=`curl -s "https://v6.ident.me/"`
+RESPONSE=`curl -s "https://www.duckdns.org/update?domains=$SUBDOMAINS&token=$TOKEN&ip=$IP&ipv6=$IPV6"`
 if [ "$RESPONSE" = "OK" ]; then
 echo "Your IP was updated at "$(date)
 else
